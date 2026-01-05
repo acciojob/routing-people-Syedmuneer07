d6506ada9f337ba30d3574d6b1af79e707b56d1f
syemie069 <syemie069@gmail.com>
2026-01-05 10:54:01 +0530

    1th commit

---
+++ src/components/UserDetails.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const UserDetails = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(null); // triggers Loading...
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, [id]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+    </div>
+  );
+};
+
+export default UserDetails;
