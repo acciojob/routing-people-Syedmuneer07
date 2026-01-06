fb7802c787999704892eab99df4252ce2f836658
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2026-01-06 21:42:30 +0530

    Update UserDetails.js

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import 'regenerator-runtime/runtime'
 
-const UserDetails = () => {
+function UserDetails() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        setUser(data);
-        setLoading(false);
-      });
-  }, [id]);
+    async function fetchUser() {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      const data = await response.json();
+      setUser(data);
+    }
 
-  if (loading) {
+    
+setTimeout(()=>{
+fetchUser();
+},1000)
+  }, [id]);
+if (!user) {
     return <div>Loading...</div>;
   }
+  
 
   return (
     <div>
+      <h1>User Details</h1>
       <p>Name: {user.name}</p>
       <p>Username: {user.username}</p>
       <p>Email: {user.email}</p>
