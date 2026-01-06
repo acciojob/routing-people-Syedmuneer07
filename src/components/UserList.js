cc99293977893deab99c357e6194b8ff32933dc8
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2026-01-06 22:27:20 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const UserList = () => {
+function UserList = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading]=useState(true);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(data => setUsers(data));
+    async function fetchUsers(){
+      const response=await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data =await response.json();
+        setUser(data);
+      setLoading(false);
+      
+    }
+    fetchUsers();
   }, []);
-
+  if(loading)<h2>Loading...</h2>
   return (
     <div>
       <h1>User List</h1>
       <ul>
-        {users.map(user => (
+        {users.map((user) => (
           <li key={user.id}>
             <Link to={`/users/${user.id}`}>{user.name}</Link>
           </li>
